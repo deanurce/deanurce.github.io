a05ae9fb7d77b01b049ccb6049c104b6523da494
deanurce <deanurce@gmail.com>
2018-07-09 15:50:26 +0200

    Add existing file

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -9,6 +9,10 @@ function draw() {
 	background('blue');	
 	ellipse(mouseX,mouseY,40,40);
 	textSize(32);
+	fill('white');
 	text("Happy UN", 200, 200);
+	fill('pink');
+	ellipse(50, 50, 50,100);
+
 
 }
